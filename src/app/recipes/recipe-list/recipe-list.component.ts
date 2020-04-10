f8c8ba7f97b4508322e28f2ba439571152e69a9f
balani98 <balanideepanshu92@gmail.com>
2020-04-10 20:47:02 +0530

    recipe displayed from array in html

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import{Recipe} from '../recipe.model'
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
@@ -8,7 +8,11 @@ import { Component, OnInit } from '@angular/core';
 export class RecipeListComponent implements OnInit {
 
   constructor() { }
+   recipes:Recipe[]=[
+    new Recipe('chole Bhature','maida gundo,tayi me ghee daalo,fir eno daalo',
+    'https://i2.wp.com/www.vegrecipesofindia.com/wp-content/uploads/2013/06/chole-bhature-recipe31.jpg')
 
+   ]
   ngOnInit(): void {
   }
 
